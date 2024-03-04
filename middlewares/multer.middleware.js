c27c3090f56c6711739c2ff625aa7bfb4ebc1c62
Gyan Seth <67365881+Gya-Se@users.noreply.github.com>
2024-03-04 09:47:09 +0000

    multer config

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -2,15 +2,30 @@ const multer = require("multer");
 
 // MULTER FOR HANDLING FILE UPLOADS
 const storage = multer.diskStorage({
+  //HANDLING FILE DESTINATION
   destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
+
+  //HANDLING FILE NAME
   filename: function (req, file, cb) {
     cb(
       null,
       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
     );
   },
+
+  //VALIDATING FILE TYPES
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"));
+    }
+  },
+
+  // HANDLING FILE SIZE LIMIT
+  limits: { fileSize: 5000000 }, // 5MB
 });
 
 const upload = multer({ storage: storage });
